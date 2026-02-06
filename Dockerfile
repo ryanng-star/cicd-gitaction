5122573672438b729ef0b7a9feea9612f66d178a
Ryan Ng Xu Yang <ryanngxuyang@Ryans-MacBook-Air.local>
2026-02-06 15:17:09 +0800

    Adding actual code content

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM nginx:alpine
-RUN echo "<h1>Deployed via GitHub Actions & Terraform!</h1>" > /usr/share/nginx/html/index.html
--- main.tf
+++ main.tf
@@ -6,46 +6,20 @@ terraform {
     }
   }
   backend "gcs" {
-    bucket  = "ryangitactionbucket"   # <<<< REPLACE THIS (created in Step 1)
+    bucket  = "ryangithubactionbucket"   # <--- Keep the one you made earlier
     prefix  = "terraform/state"
   }
 }
 
 provider "google" {
-  project = "my-rd-ce-horng-lee"      # <<<< REPLACE THIS (from Google Cloud)
+  project = "my-rd-ce-horng-lee"       # <--- Replace this
   region  = "asia-southeast1"
 }
 
-variable "docker_image" {
-  type = string
-}
-
-resource "google_cloud_run_service" "default" {
-  name     = "my-automated-service"
-  location = "asia-southeast1"
-
-  template {
-    spec {
-      containers {
-        image = var.docker_image
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
-
-# This makes the URL public so you can click it
-resource "google_cloud_run_service_iam_member" "public_access" {
-  service  = google_cloud_run_service.default.name
-  location = google_cloud_run_service.default.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
-output "url" {
-  value = google_cloud_run_service.default.status[0].url
+# --- THE NEW SIMPLE RESOURCE ---
+# This is what Terraform will create automatically
+resource "google_storage_bucket" "auto_bucket" {
+  name          = "my-github-action-bucket-ryan-123"  # <--- Make this unique!
+  location      = "asia-southeast1"
+  force_destroy = true
 }
